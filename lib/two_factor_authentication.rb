340392b453c34592ca6f5b0a267528abb3c9de63
Ketan Sabnis <ketansabnis@gmail.com>
2016-03-14 17:29:14 +0530

    removed unwanted code to handle mongoid

--- lib/two_factor_authentication.rb
+++ lib/two_factor_authentication.rb
@@ -27,7 +27,6 @@ module TwoFactorAuthentication
   NEED_AUTHENTICATION = 'need_two_factor_authentication'
   REMEMBER_TFA_COOKIE_NAME = "remember_tfa"
 
-  autoload :Schema, 'two_factor_authentication/schema'
   module Controllers
     autoload :Helpers, 'two_factor_authentication/controllers/helpers'
   end
@@ -35,7 +34,6 @@ end
 
 Devise.add_module :two_factor_authenticatable, :model => 'two_factor_authentication/models/two_factor_authenticatable', :controller => :two_factor_authentication, :route => :two_factor_authentication
 
-require 'two_factor_authentication/orm/active_record'
 require 'two_factor_authentication/routes'
 require 'two_factor_authentication/models/two_factor_authenticatable'
 require 'two_factor_authentication/rails'
--- spec/rails_app/db/migrate/20151228230340_remove_otp_secret_key_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveOtpSecretKeyFromUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :otp_secret_key, :string
-  end
-end
